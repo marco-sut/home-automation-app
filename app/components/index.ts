1335e2cefd989d15f692329dfaaf5510d593e4eb
marco-sut <sut.marco@gmail.com>
2022-07-06 01:11:06 +0200

    renaming not found page

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,4 +2,4 @@ export * from './login/login.component';
 export * from './rooms/rooms.component';
 export * from './rooms/rooms-list/rooms-list.component';
 export * from './room-settings/room-settings.component';
-export * from './notFound/notFound.component';
+export * from './not-found/not-found.component';
